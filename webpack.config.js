f0ff43471131ddefd53bc1bcb375af21c3e15e02
Nozim Yunusov <nozimdev@gmail.com>
2019-09-16 09:21:00 +0300

    fix(build)

--- webpack.config.js
+++ webpack.config.js
@@ -60,7 +60,7 @@ module.exports = {
 	},
 	mode: 'development',
 	devServer: {
-		contentBase: path.join(__dirname, 'dist'),
+		contentBase: path.join(__dirname, 'src'),
 		compress: true,
 		port: 9000,
 		hot: true,
