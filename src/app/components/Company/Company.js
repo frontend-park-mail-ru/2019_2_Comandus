c37b218ae67a8d774d297ea0fbb5e68033bd4492
Aleksandr Kosenkov <Marveel@yandex.ru>
2019-10-29 07:17:57 +0300

    feat(settings): refactor inputs to the new style

--- src/app/components/Company/Company.js
+++ src/app/components/Company/Company.js
@@ -5,6 +5,10 @@ import { htmlToElement } from '@modules/utils';
 import { enableValidationAndSubmit } from '@modules/form/formValidationAndSubmit';
 import Frame from '@frame/frame';
 import CompanyService from '@services/CompanyService';
+import DoubleSelect from '@components/inputs/DoubleSelect/DoubleSelect';
+import Button from '@components/inputs/Button/Button';
+import TextField from '@components/inputs/TextField/TextField';
+import FieldGroup from '@components/inputs/FieldGroup/FieldGroup';
 
 export class Company extends Component {
 	constructor({ ...props }) {
@@ -12,30 +16,139 @@ export class Company extends Component {
 	}
 
 	render() {
-		const countrySelect = Frame.createComponent(Select, this._el, {
-			id: 'country',
-			name: 'country',
-			className: 'tp-input',
-			items: [
-				{
-					label: 'Выберите страну',
-					value: 'nil',
-					selected: true,
-					disabled: true,
-				},
-				{ label: 'Россия', value: 'Россия', selected: false },
-				{ label: 'Украина', value: 'Украина', selected: false },
-				{ label: 'Беларусь', value: 'Беларусь', selected: false },
-				{ label: 'Казахстан', value: 'Казахстан', selected: false },
-				{ label: 'Армения', value: 'Армения', selected: false },
-			],
-			onChange(value) {
-				console.log('change: ', value);
-			},
+		// const countrySelect = Frame.createComponent(Select, this._el, {
+		// 	id: 'country',
+		// 	name: 'country',
+		// 	className: 'tp-input',
+		// 	items: [
+		// 		{
+		// 			label: 'Выберите страну',
+		// 			value: 'nil',
+		// 			selected: true,
+		// 			disabled: true,
+		// 		},
+		// 		{ label: 'Россия', value: 'Россия', selected: false },
+		// 		{ label: 'Украина', value: 'Украина', selected: false },
+		// 		{ label: 'Беларусь', value: 'Беларусь', selected: false },
+		// 		{ label: 'Казахстан', value: 'Казахстан', selected: false },
+		// 		{ label: 'Армения', value: 'Армения', selected: false },
+		// 	],
+		// 	onChange(value) {
+		// 		console.log('change: ', value);
+		// 	},
+		// });
+
+		this._citySelect = new DoubleSelect({
+			name: 'city',
+		});
+		const submitBtn = new Button({
+			type: 'submit',
+			text: 'Сохранить изменения',
 		});
 
+		const titleField = new TextField({
+			required: true,
+			name: 'companyName',
+			type: 'text',
+			label: 'Название',
+			placeholder: 'Технопарк',
+			minlength: '3',
+			maxlength: '20',
+		});
+		const siteField = new TextField({
+			required: false,
+			name: 'site',
+			type: 'text',
+			label: 'Сайт',
+			placeholder: 'park.mail.ru',
+			minlength: '5',
+			maxlength: '30',
+		});
+		const headerField = new TextField({
+			required: true,
+			name: 'tagline',
+			type: 'text',
+			label: 'Заголовок описания',
+			placeholder: 'Проект',
+			minlength: '5',
+			maxlength: '60',
+		});
+		const descriptionField = new TextField({
+			required: true,
+			name: 'description',
+			type: 'textarea',
+			label: 'Описание',
+			placeholder: 'Описание компании',
+		});
+
+		const ownerField = new TextField({
+			required: false,
+			readonly: true,
+			name: 'companyOwner',
+			type: 'text',
+			label: 'Владелец',
+			placeholder: 'Владелец',
+			value: 'Хлоя Прайс',
+		});
+
+		const addressField = new TextField({
+			required: false,
+			name: 'address',
+			type: 'text',
+			label: 'Адрес',
+			placeholder: 'Бауманская 7',
+			minlength: '5',
+			maxlength: '40',
+		});
+
+		const phoneField = new TextField({
+			required: true,
+			name: 'phone',
+			type: 'text',
+			label: 'Телефон',
+			pattern: '^+[0-9]{11,12}$',
+			title:
+				'Неправильный формат номера телефона. Пример: +7 900 90 90 900',
+			placeholder: '+78005553535',
+		});
+
+		this.data = {
+			citySelect: this._citySelect.render(),
+			siteField: new FieldGroup({
+				children: [siteField.render()],
+				label: 'Сайт компании',
+			}).render(),
+			ownerField: new FieldGroup({
+				children: [ownerField.render()],
+				label: 'Владелец',
+			}).render(),
+			titleField: new FieldGroup({
+				children: [titleField.render()],
+				label: 'Название',
+			}).render(),
+			headerField: new FieldGroup({
+				children: [headerField.render()],
+				label: 'Заголовок описания',
+			}).render(),
+			descriptionField: new FieldGroup({
+				children: [descriptionField.render()],
+				label: 'Описание',
+			}).render(),
+			addressField: new FieldGroup({
+				children: [addressField.render()],
+				label: 'Адрес компании',
+			}).render(),
+			phoneField: new FieldGroup({
+				children: [phoneField.render()],
+				label: 'Телефон компании',
+			}).render(),
+			submitBtn: new FieldGroup({
+				children: [submitBtn.render()],
+			}).render(),
+		};
+
 		const html = template({
-			countrySelect: countrySelect.render(),
+			// countrySelect: countrySelect.render(),
 			data: this.data,
 			props: this.props,
 		});
