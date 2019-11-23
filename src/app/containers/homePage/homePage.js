7ba6e0b02cd0add69bc2d1ec0193f65d9edad876
Nozim Yunusov <nozimdev@gmail.com>
2019-11-24 00:17:25 +0300

    feat(main page) new landing page design

--- src/app/containers/homePage/homePage.js
+++ src/app/containers/homePage/homePage.js
@@ -5,6 +5,7 @@ import './style.css';
 // import './chaice.scss';
 // import './pulsingCircles.scss';
 // import './shiningText.scss';
+import BenzeneParticles from './benzene';
 
 class HomeComponent extends Component {
 	constructor({ parent = document.body, ...props }) {
@@ -16,6 +17,24 @@ class HomeComponent extends Component {
 		const html = template(this.data);
 		const el = htmlToElement(html);
 		this._parent.appendChild(el);
+		BenzeneParticles(document.getElementById('block__pulticles'), {
+			edgeLightUpSpeed: 0.002,
+			edgeFadeSpeed: 0.001,
+			edgeLightUpBrightness: 0.02,
+			edgeLightUpAlphaDiff: 0.002,
+			eraseAlpha: 0.5,
+			trailSize: 35,
+			pulseChance: 0.06,
+			maxPulsesPerSpawn: 1,
+			maxPulses: 60,
+			minVertexRadius: 3,
+			minPulseSpeed: 0.03 / 2.25,
+			pulseSpeedVariation: 0.04 / 2.25,
+			vertexRadiusVariation: 3,
+			spacing: 100,
+			bg: [43, 50, 58],
+			fg: [31, 124, 231],
+		});
 	}
 }
 
